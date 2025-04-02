1a43d4fbd47e4a59fe18225116c77e9433b27a21
Bohdan <bodya24102001@gmail.com>
2025-04-02 18:15:12 +0300

    cors origin addresses expanded

--- server/src/config/cors.config.ts
+++ server/src/config/cors.config.ts
@@ -1,7 +1,14 @@
 export function createCorsConfig() {
   return {
-    origin: `http://localhost:${process.env.CLIENT_PORT ?? 3000}`,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      `http://localhost:${process.env.CLIENT_PORT ?? 3000}`,
+      'http://localhost',
+      'http://bbilokin.stud.shpp.me',
+      'https://bbilokin.stud.shpp.me',
+      'http://bbilokin.duckdns.org/',
+      'https://bbilokin.duckdns.org/',
+    ],
+    methods: 'GET,PUT,POST,DELETE',
     credentials: true,
   };
 }
